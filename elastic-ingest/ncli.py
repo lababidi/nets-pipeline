7d087072ff897ab66703695cd91081240823be17
David Sherman <sherman@thehumangeo.com>
2016-07-19 16:57:33 -0400

    setting up for cloud foundry deployment

--- elastic-ingest/ncli.py
+++ elastic-ingest/ncli.py
@@ -6,9 +6,10 @@ from  eventmapping import eventmapping
 from pipeline import Pipeline
 from elasticsearch import Elasticsearch, ElasticsearchException
 from elasticsearch.client import IndicesClient
-from os import listdir
+from os import listdir,getenv, system
 from os.path import isfile, join
 from time import sleep
+import sys
 
 
 class Ncli:
@@ -16,20 +17,28 @@ class Ncli:
     _yaml = 'nets.yaml'
 
     def __init__(self, yamlFile):
+
         with open(yamlFile) as f:
             self.parameters = yaml.load(f)
+
+        #if environment variables are provided for elasticsearch host and port
+        #use those instead
+
+        for key in ['HOST', 'PORT']:
+            v = getenv( 'NCLI_ES_%s' % key )
+            if v is not None:
+                self.parameters['elasticsearch'][key.lower()] = v
 
         # todo - log to stdout AND file
         logging.basicConfig(format=self.parameters['logging']['format'])
         self.logger = logging.getLogger("NETS")
         self.logger.level = logging.INFO
-        self.eventpipeline = Pipeline(self.parameters)
 
-        self.es_url = 'http://%s:%s' % (
-        self.parameters['elasticsearch']['host'], self.parameters['elasticsearch']['port'])
-        self.logger.info("ready. ES URL : %s", self.es_url)
+
         try:
-            self.es = Elasticsearch()
+            self.es = Elasticsearch(hosts=[
+                {'host': self.parameters['elasticsearch']['host'],
+                 'port' : self.parameters['elasticsearch']['port']}])
             info = self.es.info()
             self.logger.info("Connected to Elasticsearch v. %s, name: %s" % (info['version']['number'], info['name']))
 
@@ -41,6 +50,8 @@ class Ncli:
         for item in self.parameters['elasticsearch']['indexes']:
             if item['type'] == target:
                 return (item['name'], item['doctype'])
+
+    # display status check and exit
 
     def status(self):
         idx_client = IndicesClient(self.es)
@@ -55,6 +66,8 @@ class Ncli:
             else:
                 self.logger.info("%s does not exist" % es_index)
 
+    # initialize articles or events index.
+
     def initialize(self, idx):
         es_index, es_doctype = self.indexinfo(idx)
         self.logger.info("Initializing %s" % es_index)
@@ -65,7 +78,10 @@ class Ncli:
         if idx == 'event': idx_client.put_mapping(doc_type=es_doctype, index=[es_index], body=eventmapping())
         self.logger.info("%s ready." % es_index)
 
+    # find n articles and rum them through the pipeline
+
     def pipeline(self, n):
+        self.eventpipeline = Pipeline(self.parameters)
         es_index, es_doctype = self.indexinfo('article')
         self.logger.info("Send %s articles through the pipeline" % n)
         query =  '{"query": { "bool": { "must_not": { "exists": { "field": "status" }}}}}'
@@ -82,6 +98,8 @@ class Ncli:
             self.logger.info("..................")
             sleep(delay)
 
+    # load articles from json files in a directory
+
     def load(self):
         self.logger.info("Load articles")
         es_index, es_doctype = self.indexinfo('article')
@@ -94,6 +112,14 @@ class Ncli:
                     if '_id' in article: del article['_id']
                     self.es.index(index=es_index, doc_type=es_doctype, body=article)
 
+    # install english language pack and quit
+
+    def languagepack(self):
+        self.logger.info("Initialize spacy EN language pack...")
+        command = 'python -m spacy.en.download'
+        system( command)
+        self.logger.info("... done")
+
 
 parser = argparse.ArgumentParser(description='NETS Command Line Interface. v %s' % Ncli._version)
 parser.add_argument('--yaml', default='nets.yaml', help='YAML file')
@@ -102,9 +128,17 @@ parser.add_argument('--load', default=None, help='load raw article files', actio
 parser.add_argument('--export', default=None, help='export up to 100 events with given type')
 parser.add_argument('--pipeline', default=None, help='Run the pipeline for up to given number of articles')
 parser.add_argument('--initialize', default='NA', help='Initialize an index', choices=['article', 'event'])
+parser.add_argument('--languagepack', help='Load Enlglish language pack', action='store_true')
 
 args = parser.parse_args()
 me = Ncli(args.yaml)
+
+# if no arguments on command line, pick up command from environment.
+
+if ( len(sys.argv) == 1 ):
+    command = getenv('NCLI_COMMAND')
+    if command == 'status': args.status = True
+    if command == 'pipeline': args.pipeline = 200
 if args.status:
     me.status()
 elif not args.initialize == 'NA':
@@ -115,3 +149,6 @@ elif args.export:
     me.export(args.export)
 elif args.pipeline:
     me.autopipeline(args.pipeline)
+elif args.languagepack:
+    me.languagepack()
+
