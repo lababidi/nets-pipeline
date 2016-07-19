d9f3727aef2308aaad9beca990e1f67e50b7e065
Syrus Mesdaghi <syrusm@prominentedge.com>
2016-07-19 18:08:10 -0400

    support query_string as opposed to having to always specify keywords. fixed a little bug with person vs people. updated a bunch of js hints related issues.

--- eq.js
+++ eq.js
@@ -1,77 +1,82 @@
 
 //TODO - get these two  from a configuration file.  Hard coded here.
 
-function eventindex() { return "nets-event"}
-function eventdoctype() { return "event"}
+function eventIndex() { return "nets-event"}
+function eventDocType() { return "event"}
 
-function eventurl( fieldlist )
+function eventUrl( fieldlist )
 {
-    url =  eventindex() + "/" + eventdoctype()  + "/_search"
+    var url =  eventIndex() + "/" + eventDocType()  + "/_search";
     if (fieldlist.length > 0)
-        url = url + "?_source=" + fieldlist
+        url = url + "?_source=" + fieldlist;
     return url
 }
-function query( keywords,start_date, end_date, places, people, organizations, offset, size )
+
+function query( query_string, only_search_title, start_date, end_date, places, people, organizations, offset, size )
 {
-    payload = {
-        "from" : offset,
-        "size" : size,
-        "query" : {
-            "filtered" : {
-                "query" : {
-                    "multi_match" : {
-                        "query" : keywords,
-                        "fields" : ["title", 'content']
-                  }
+    var payload = {
+        "from": offset,
+        "size": size,
+        "query": {
+            "filtered": {
+                "query": {
+                    "query_string": {
+                        "query": query_string,
+                        "analyze_wildcard": true,
+                        "fields": only_search_title? ['title']: ['title', 'content']
+                    }
                 },
-                "filter" : [
-                { "range" : { "date_published" : {
-                    "gte" : start_date,
-                    "le" : end_date }
-                    }}
+                "filter": [
+                    {
+                        "range": {
+                            "date_published": {
+                                "gte": start_date,
+                                "le": end_date
+                            }
+                        }
+                    }
                 ]
-             }
+            }
         }
-    }
+    };
 
     if ( places.length  > 0 ) {
-        for (place in places)
+        for (var place in places)
             payload.query.filtered.filter.push( { "term" : { "places" : places[place] }})
     }
     if ( people.length  > 0 ) {
-        for (person in people)
-            payload.query.filtered.filter.push( { "term" : { "person" : people[person] }})
+        for (var person in people)
+            payload.query.filtered.filter.push( { "term" : { "people" : people[person] }})
     }
     if ( organizations.length  > 0 ) {
-        for (org in organizations)
+        for (var org in organizations)
             payload.query.filtered.filter.push( { "term" : { "organizations" : organizations[org] }})
     }
 
     return payload
 }
 
-function significant_terms( keywords, start_date, end_date, places, people, organizations)
+function significant_terms( query_string, only_search_title, start_date, end_date, places, people, organizations)
 {
-    payload = query(keywords, start_date, end_date, places, people, organizations, 0, 0)
-    payload['size'] = 0
+    var payload = query( query_string, only_search_title, start_date, end_date, places, people, organizations, 0, 0);
+    payload['size'] = 0;
     payload['aggs'] =   {
         "significantPlaces" : { "significant_terms" : { "field" : "places" } },
         "significantPeople" : { "significant_terms" : { "field" : "people" } }
-    }
+    };
 
     return payload
 }
 
-function aggregates( keywords, start_date, end_date, places, people, organizations)
+function aggregates( query_string, only_search_title, start_date, end_date, places, people, organizations)
 {
-    payload = query(keywords, start_date, end_date, places, people, organizations, 0,0 )
-    payload['size'] = 0
+    var payload = query(query_string, only_search_title, start_date, end_date, places, people, organizations, 0, 0 );
+    payload['size'] = 0;
     payload['aggs'] = {
         "places" : { "terms" : { "field" : "places" }},
         "people" : { "terms" : { "field" : "people" }},
-        "organizations" : { "terms" : { "field" : "organizations" }},
-
-    }
+        "organizations" : { "terms" : { "field" : "organizations" }}
+    };
 
     return payload
 }
